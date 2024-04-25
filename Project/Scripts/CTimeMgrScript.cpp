f536a5b1eadb547b9e4b00f63873eec3698041ef
superozing <amazingozing@gmail.com>
2024-04-25 22:00:19 +0900

    #11 refactor / Set 함수에서 바로 TimeMgr에게 함수 호출

--- Project/Scripts/CTimeMgrScript.cpp
+++ Project/Scripts/CTimeMgrScript.cpp
@@ -28,8 +28,20 @@ void CTimeMgrScript::begin()
 void CTimeMgrScript::tick()
 {
 	// 만약 ScriptParam에 함수를 호출하는 델리게이트 형식의 버튼이 있다면 매 틱마다 함수를 호출하지 않아도 될 것 같다.
-	TIMEMGR->LockDeltaTime(m_DTLock);
+	LockDeltaTime(m_DTLock);
+	SetDTScale(m_DTScale);
+}
+
+void CTimeMgrScript::SetDTScale(float _DTScale)
+{
+	m_DTScale = _DTScale;
 	TIMEMGR->SetDTScale(m_DTScale);
+}
+
+void CTimeMgrScript::LockDeltaTime(bool _DTLock)
+{
+	m_DTLock = _DTLock;
+	TIMEMGR->LockDeltaTime(m_DTLock);
 }
 
 void CTimeMgrScript::SaveToFile(FILE* _File)
--- Project/Scripts/CTimeMgrScript.h
+++ Project/Scripts/CTimeMgrScript.h
@@ -9,8 +9,8 @@ private:
 	int		m_DTLock; // 원래라면 bool 형식을 사용해야 하지만, 현재 스크립트 파람에 bool이 없기에 int를 사용함.
 
 public:
-	void SetDTScale(float _DTScale) { m_DTScale = _DTScale; }
-	void LockDeltaTime(bool _DTLock) { m_DTLock = _DTLock; }
+	void SetDTScale(float _DTScale);
+	void LockDeltaTime(bool _DTLock);
 
 public:
 	virtual void begin() override;
